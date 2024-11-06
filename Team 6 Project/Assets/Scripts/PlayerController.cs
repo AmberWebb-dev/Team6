b17442f551ee19950b46edc81c9e58d9abbb2436
Amber Webb <webbamber.dev@gmail.com>
2024-11-06 01:30:14 -0500

    Commit for player to have ability to shoot as well as adding a reticle to the screen

--- Team 6 Project/Assets/Scripts/PlayerController.cs
+++ Team 6 Project/Assets/Scripts/PlayerController.cs
@@ -4,18 +4,24 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField] LayerMask ignoreMask;
     [SerializeField] CharacterController pController;
 
     [SerializeField] int speed;
     [SerializeField] int sprint;
+
     [SerializeField] int jumpMax;
     [SerializeField] int jumpSpeed;
     [SerializeField] int gravity;
 
+    [SerializeField] int shootDamage;
+    [SerializeField] float shootRate;
+    [SerializeField] int shootDistance;
+
     private int jumpCount;
 
     bool isSprinting;
-    bool isJumping;
+    bool isShooting;
 
     Vector3 moveDirection;
     Vector3 playerVelocity;
@@ -29,6 +35,8 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // Show the shooting distance on the scene screen
+        Debug.DrawRay(Camera.main.transform.position, Camera.main.transform.forward * shootDistance, Color.red);
         Movement();
         Sprint();
     }
@@ -62,6 +70,11 @@ public class PlayerController : MonoBehaviour
 
         pController.Move(playerVelocity * Time.deltaTime);
         playerVelocity.y -= gravity * Time.deltaTime;
+
+        if(Input.GetButton("Fire1") && !isShooting)
+        {
+            StartCoroutine(Shoot());
+        }
     }
     
     void Jump()
@@ -85,5 +98,29 @@ public class PlayerController : MonoBehaviour
             speed /= sprint;
             isSprinting = false;
         }
+    }
+
+    IEnumerator Shoot()
+    {
+        isShooting = true;
+
+        RaycastHit hit;
+        if(Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, shootDistance, ~ignoreMask))
+        {
+            // Show what the player is shooting on the Debug Log
+            Debug.Log(hit.collider.name);
+
+            IDamage dmg = hit.collider.GetComponent<IDamage>();
+
+            // If damage is not null set damage amount to shoot damage
+            if(dmg != null )
+            {
+                dmg.TakeDamage(shootDamage);
+            }
+        }
+
+        // Wait for duration of the shooting rate and set isShooting back to false
+        yield return new WaitForSeconds(shootRate);
+        isShooting = false;
     }
 }
