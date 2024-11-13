8a1122691c72291635ce52d63c029b7f3217d514
Amber <webbamber.dev@gmail.com>
2024-11-12 21:36:03 -0500

    Commit to add respawn to health pack

--- Team 6 Project/Assets/Scripts/HealthPack.cs
+++ Team 6 Project/Assets/Scripts/HealthPack.cs
@@ -4,12 +4,14 @@ using UnityEngine;
 
 public class HealthPack : MonoBehaviour
 {
+    [SerializeField] float packRespawnTimer;
 
+    Vector3 origPos;
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        origPos = transform.position;
     }
 
     public void OnTriggerEnter(Collider other)
@@ -22,7 +24,17 @@ public class HealthPack : MonoBehaviour
         if (health != null)
         {
             health.HealUp();
-            Destroy(gameObject);
+            // Move health pack off the map
+            Vector3 gonePos = new Vector3(0, -10, 0);
+            gameObject.transform.position = gonePos;
+            StartCoroutine(RespawnHealthPack());
         }
+    }
+
+    IEnumerator RespawnHealthPack()
+    {
+        yield return new WaitForSeconds(packRespawnTimer);
+        // Return pack back to original position
+        gameObject.transform.position = origPos;
     }
 }
