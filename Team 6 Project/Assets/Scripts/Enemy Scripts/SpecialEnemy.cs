7793e678d0695a96ba75bffbb8f061a7a3ad4a90
Amber <webbamber.dev@gmail.com>
2024-11-14 20:42:01 -0500

    commit to add roam to special enemy

--- Team 6 Project/Assets/Scripts/Enemy Scripts/SpecialEnemy.cs
+++ Team 6 Project/Assets/Scripts/Enemy Scripts/SpecialEnemy.cs
@@ -16,34 +16,91 @@ public class SpecialEnemy : MonoBehaviour, IDamage
     [SerializeField] LayerMask enemyLayer;
     [SerializeField] LayerMask playerLayer;
     [SerializeField] AudioClip explosionSound;
+    [SerializeField] int roamDistance;
+    [SerializeField] int roamTimer;
 
     [SerializeField] int scoreValue;
 
     bool playerInRange;
-    bool hasExploded = false; 
+    bool hasExploded = false;
+    bool isRoaming;
+
     Vector3 playerDir;
+    Vector3 startPosition;
+
     float angleToPlayer;
+    float stoppingDistanceOrig;
 
     Color ogColor;
     private AudioSource audioSource;
 
     void Start()
     {
+        agent = GetComponent<NavMeshAgent>();
         ogColor = model.material.color;
         audioSource = GetComponent<AudioSource>();
         GameManager.Instance.GameGoal(1);
+        startPosition = transform.position;
+        stoppingDistanceOrig = agent.stoppingDistance;
     }
 
     void Update()
     {
-        if (playerInRange)
+        if (playerInRange && !CanSeePlayer())
         {
+            if (!isRoaming && agent.remainingDistance < 0.05f)
+            {
+                StartCoroutine(Roam());
+            }
+        }
+        else if (!playerInRange)
+        {
+            if (!isRoaming && agent.remainingDistance < 0.05f)
+            {
+                StartCoroutine(Roam());
+            }
+        }
+
+    }
+
+    IEnumerator Roam()
+    {
+        isRoaming = true;
+        yield return new WaitForSeconds(roamTimer);
+
+        agent.stoppingDistance = 0;
+        Vector3 randDistance = Random.insideUnitSphere * roamDistance;
+        randDistance += startPosition;
+
+        NavMeshHit hit;
+        NavMesh.SamplePosition(randDistance, out hit, roamDistance, 1);
+        agent.SetDestination(hit.position);
+
+        isRoaming = false;
+    }
+    bool CanSeePlayer()
+    {
+        playerDir = GameManager.Instance.player.transform.position - headPos.position;
+        angleToPlayer = Vector3.Angle(playerDir, transform.position);
+
+        RaycastHit hit;
+        if (Physics.Raycast(headPos.position, playerDir, out hit))
+        {
+            // Player seen
             agent.SetDestination(GameManager.Instance.player.transform.position);
+
             if (agent.remainingDistance <= agent.stoppingDistance)
             {
                 FaceTarget();
             }
+            
+
+            agent.stoppingDistance = stoppingDistanceOrig;
+            return true;
         }
+
+        agent.stoppingDistance = 0;
+        return false;
     }
 
     private void OnTriggerEnter(Collider other)
@@ -59,13 +116,13 @@ public class SpecialEnemy : MonoBehaviour, IDamage
         if (other.CompareTag("Player"))
         {
             playerInRange = false;
-            agent.SetDestination(transform.position);
+            //agent.SetDestination(transform.position);
+            agent.stoppingDistance = 0;
         }
     }
 
     void FaceTarget()
     {
-        playerDir = GameManager.Instance.player.transform.position - transform.position;
         Quaternion rot = Quaternion.LookRotation(playerDir);
         transform.rotation = Quaternion.Lerp(transform.rotation, rot, Time.deltaTime * faceTargetSpeed);
     }
