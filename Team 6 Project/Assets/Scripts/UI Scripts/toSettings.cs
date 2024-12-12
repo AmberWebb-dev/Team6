d13df5f51db31e35bf74f24a23129e7f17e1122b
dxstbxnny <TRNeal@student.fullsail.edu>
2024-12-12 02:24:19 -0500

    settings button works

---
+++ Team 6 Project/Assets/Scripts/UI Scripts/toSettings.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class toSettings : MonoBehaviour
+{
+    public void LoadSettingsScene()
+    {
+        SceneManager.LoadScene("Settings");
+
+    }
+}
