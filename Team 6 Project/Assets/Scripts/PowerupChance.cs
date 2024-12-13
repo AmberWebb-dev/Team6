35fa0a2ef7cd300f105899ea81ce83df580d0dcd
YabDev <ethan.yabsley@gmail.com>
2024-12-12 17:18:15 -0800

    Golden shovel now sometimes drops in endless mode

--- Team 6 Project/Assets/Scripts/PowerupChance.cs
+++ Team 6 Project/Assets/Scripts/PowerupChance.cs
@@ -1,19 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 [System.Serializable]
 public class PowerupChance
 {
     public GameObject powerupPrefab;
     public float chanceToSpawn;
+    public int sceneLock = -1;
 
     public bool RollChance()
     {
-        float r = Random.Range(0.0f, 1.0f);
-        if (r <= chanceToSpawn)
+        if (sceneLock == -1 || sceneLock == SceneManager.GetActiveScene().buildIndex)
         {
-            return true;
+            float r = Random.Range(0.0f, 1.0f);
+            if (r <= chanceToSpawn)
+            {
+                return true;
+            }
         }
         return false;
     }
