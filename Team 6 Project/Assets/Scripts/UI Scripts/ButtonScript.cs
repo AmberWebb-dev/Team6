0f950f9115bd3300d75dac43d4d3559d4367e0ef
Amber Webb <webbamber.dev@gmail.com>
2024-12-19 21:57:20 -0500

    Commit to fix quit button in WebGL

--- Team 6 Project/Assets/Scripts/UI Scripts/ButtonScript.cs
+++ Team 6 Project/Assets/Scripts/UI Scripts/ButtonScript.cs
@@ -1,10 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class ButtonScript : MonoBehaviour
 {
+    GameObject[] quitButtons;
+    GameObject mainMenuPlay;
+    GameObject mainMenuSettings;
+    Vector3 webGLPlayPos;
+
+    private void Start()
+    {
+        if (Application.platform == RuntimePlatform.WebGLPlayer)
+        {
+            quitButtons = GameObject.FindGameObjectsWithTag("Quit Button");
+            foreach (GameObject button in quitButtons)
+            {
+                button.SetActive(false);
+            }
+            mainMenuPlay = GameObject.FindGameObjectWithTag("MMPlayButton");
+            mainMenuSettings = GameObject.FindGameObjectWithTag("MMSettingsButton");
+            webGLPlayPos = mainMenuPlay.transform.position;
+            webGLPlayPos.x = mainMenuSettings.transform.position.x;
+            mainMenuPlay.transform.position = webGLPlayPos;
+        }
+    }
+    private void Update()
+    {
+        if (Application.platform == RuntimePlatform.WebGLPlayer)
+        {
+            quitButtons = GameObject.FindGameObjectsWithTag("Quit Button");
+            foreach (GameObject button in quitButtons)
+            {
+                button.SetActive(false);
+            }
+        }
+    }
     public void Resume()
     {
         GameManager.Instance.StateUnpause();
@@ -26,11 +59,13 @@ public class ButtonScript : MonoBehaviour
     }
     public void Quit()
     {
+
 #if UNITY_EDITOR
         UnityEditor.EditorApplication.isPlaying = false;
 #else
         Application.Quit();
 #endif
+
     }
 
     public void NextLevel()
